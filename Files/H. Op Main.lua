7f51f3bc36aa06f24165c69e85b1118d31f6cecc
Hd8Dev <tymekradeksikorski1@gmail.com>
2024-11-10 17:41:51 +0100

    Update H. Op Main.lua

--- Files/H. Op Main.lua
+++ Files/H. Op Main.lua
@@ -1,3 +1,7 @@
-loadstring(game:HttpGet((
+loadstring(game:HttpGet((https://raw.githubusercontent.com/Hd8Dev/JoLo-Development/refs/heads/main/Files/H.OpMakeUser.lua)))()
 
 
+wait(10)
+loadstring(game:HttpGet((https://raw.githubusercontent.com/Hd8Dev/JoLo-Development/refs/heads/main/JoLoDevelopment/OwnerInterface.lua)))()
+
+.
