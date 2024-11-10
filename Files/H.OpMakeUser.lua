2cf8ef0232623fc2c835c432937e7326cff780b8
Hd8Dev <tymekradeksikorski1@gmail.com>
2024-11-10 17:37:18 +0100

    Update H.OpMakeUser.lua

--- Files/H.OpMakeUser.lua
+++ Files/H.OpMakeUser.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet((https://raw.githubusercontent.com/Hd8Dev/JoLo-Development/refs/heads/main/Files/H.%20Op.lua))()
+loadstring(game:HttpGet((https://raw.githubusercontent.com/Hd8Dev/JoLo-Development/refs/heads/main/Files/H.%20Op.lua)))()
+
+
+pcall:Warning()
