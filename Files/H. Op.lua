e2f24572905c3af53087cbb44b50a28f6595fdd4
Hd8Dev <tymekradeksikorski1@gmail.com>
2024-11-10 15:24:36 +0100

    Create H. Op.lua

---
+++ Files/H. Op.lua
@@ -0,0 +1,23 @@
+
+
+local PL1 = Players.LocalPlayer
+local Name = PL1.Name
+  function pcall:Warning()
+  print(Name)
+  if Name ~= "tymkot12345" then
+print('JoLo Development Info')
+    print('Client Sided 🔨')
+     print('We recived an error about your username ⚠')
+     print('Wait untill we check ur username')
+     wait(10)
+     print('Checking.❓')
+wait(1)
+     print('Checking..❓')
+    wait(2)
+     print('Checking...❓')
+    wait(1)
+     print('This interface is owner only | <404 Input>')
+  else
+    print("Loading...")
+  end
+end
